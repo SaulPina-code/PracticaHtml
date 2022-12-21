b428142f09a278dbd28d2369b5ac7ddee9c35233
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2022-12-20 21:10:13 -0300

    tarea y correccion de tabla css

---
+++ tabla y formulario/taera.js
@@ -0,0 +1,22 @@
+/* 
+tarea del 20/12 para el 22/12
+IDEA DE LA PAGINA : pagina que puedas ver noticias de los libros 
+"Pagina principal"
+ crear un navbar 
+ crear una parte de contenido que hables sobre libros mas vendidos 
+ tiene que contener una foto, un texto y un boton
+crear un footer 
+"pagina secundaria"
+crar un navbar exactamente igual al anterior 
+un formulario para crear nuevas noticias
+***
+nombre del libro
+cantidad de paginas 
+email del que creo el libro 
+fecha que se creo el libro 
+url de una foto de un libro 
+boton de cargar (tiene que decir )
+***
+
+crear un footer
+*/
